972fc540187d138bcf5550bfff7704303a9ec943
pierre veillet <pierre.veillet38@gmail.com>
2024-04-11 23:04:02 +0200

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    files: ["src/**/*.js"],
+    rules: {
+      "prefer-const": "error",
+      "no-unused-vars": "error",
+      "prettier/prettier": "warn",
+    },
+    languageOptions: {
+      ecmaVersion: 7,
+      sourceType: "module",
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
